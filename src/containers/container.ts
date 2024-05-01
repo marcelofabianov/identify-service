1ab20c767b9af7f310d56059e65a9b9e876023a3
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-05-01 07:31:41 -0300

    refactor: validando chaves do container ao usar add ou get

--- src/containers/container.ts
+++ src/containers/container.ts
@@ -6,10 +6,22 @@ export class Container {
     }
 
     add(key: string, value: unknown): void {
+        if (this.has(key)) {
+            throw new Error(`Key ${key} already exists in container`)
+        }
+
         this.container[key] = value
     }
 
     get(key: string): unknown {
+        if (!this.has(key)) {
+            throw new Error(`Key ${key} not found in container`)
+        }
+
         return this.container[key]
+    }
+
+    has(key: string): boolean {
+        return this.container[key] !== undefined
     }
 }
